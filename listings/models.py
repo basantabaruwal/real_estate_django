eeed670c142de69baa466348fc05b3d8d5853c70
Basanta Baruwal <baruwalbasanta@gmail.com>
2020-01-30 13:09:11 +0545

    Realtors Model created

--- listings/models.py
+++ listings/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from realtors.models import Realtor
+from datetime import datetime
 
 # Create your models here.
 class Listing(models.Model):
@@ -28,7 +29,7 @@ class Listing(models.Model):
     photo_5 = models.ImageField(upload_to='photos/%Y%m%d/', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y%m%d/', blank=True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateTimeField(auto_now=True, blank=True)
+    list_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
--- realtors/models.py
+++ realtors/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Realtor(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to="images/%Y%m%d/")
+    description = models.TextField(blank=True)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
