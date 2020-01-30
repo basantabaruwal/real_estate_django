594a2e20ebb6079a793aba7bdcc8646258b32a79
Basanta Baruwal <baruwalbasanta@gmail.com>
2020-01-30 12:08:04 +0545

    Some Minor Changes

--- listings/views.py
+++ listings/views.py
@@ -4,6 +4,7 @@ from django.utils.text import slugify
 # Create your views here.
 
 def listings(request):
+    print("Listings: ", request.path)
     return render(request, 'listings/listings.html')
 
 def listing(request, listing_slug):
