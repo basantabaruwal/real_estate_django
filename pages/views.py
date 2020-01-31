19f92fa1992b54f0100927b1325d0c2448b0c229
Basanta Baruwal <baruwalbasanta@gmail.com>
2020-01-31 08:14:58 +0545

    Minor Changes

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from listings.models import Listing
 
-# Create your views here.
 
 def home(request):
-    return render(request, 'pages/index.html')
+    latest_listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    data = {
+        'listings': latest_listings
+    }
+    return render(request, 'pages/index.html', data)
 
 
 def about(request):
